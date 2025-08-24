01aa1b057e95231cecc15199860ff6c021d11eee
yagakeerthikiran <91780577+yagakeerthikiran@users.noreply.github.com>
2025-08-24 22:46:08 +1000

    Create sign in page

---
+++ frontend/src/app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '@/components/AuthForm'
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <AuthForm mode="signin" />
+    </div>
+  )
+}
